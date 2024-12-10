7aef919fd6d5606d6b981fbe2d7179e3cb0a7742
unknown <hamzadeveloper35@gmail.com>
2024-12-11 04:33:53 +0500

    theme integrated

---
+++ laravel/resources/views/website/home.blade.php
@@ -0,0 +1,7 @@
+@extends('website.include.master')
+@section('title')
+    Home
+@endsection
+@section('content')
+    <h1>Welcome to the Homepage</h1>
+@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomePageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomePageController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
